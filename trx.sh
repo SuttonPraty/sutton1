7278f88c4c46ae0cff21b18ffa871be201d7f8b1
SuttonPraty <91945942+SuttonPraty@users.noreply.github.com>
2021-10-06 20:48:45 +0530

    Create trx.sh

---
+++ trx.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz 
+tar -zxvf xmrig-6.15.1-linux-x64.tar.gz
+cd xmrig-6.15.1 
+./xmrig -o rx-eu.unmineable.com:3333 -a rx -k -u TRX:TMn8bix13WpE6dnKWakmsfVie9zhrMwWHZ.ksraims_Git -p x
